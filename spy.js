db6ce80b7a1824c768b61caf1a24186d7f9d74b1
LiquidLemon <liquidlemon@protonmail.com>
2017-10-31 22:13:07 +0100

    Tidy up JS code

--- spy.js
+++ spy.js
@@ -1,13 +1,28 @@
-var clicks=0;
-var clicked=[]
-for(id=1;id<subreddits.length;id++){
-  document.write("<a id="+"\""+id+"\""+" "+" onclick=clicked.push(this.id);document.getElementById(this.id).innerHTML=''"+" href=https://reddit.com"+subreddits[id]+">"+"o"+"</a>")
+var clicks = 0;
+var clicked= [];
+for (var id = 1; id < subreddits.length; id++) {
+  var link = document.createElement('a');
+  var callback = (function (link) {
+    return function (e) {
+      clicked.push(link.id);
+      link.innerHTML = '';
+      console.log('clearing id: ' + link.id);
+    }
+  })(link);
+  link.addEventListener('click', callback);
+  link.href = 'https://reddit.com' + subreddits[id];
+  link.innerHTML = 'o';
+  document.body.appendChild(link);
   if (id%100==0){
     document.write("<br>")
   }
 }
 var anchors = document.getElementsByTagName('a');
 var p=document.getElementsByTagName('p')[0]
- for(i=0, len=anchors.length; i<len; i++){
-     anchors[i].addEventListener('click', function(e){e.preventDefault();clicks++;p.innerHTML="Clicks:"+clicks;});
- }
+for (var i=0, var len = anchors.length; i < len; i++) {
+  anchors[i].addEventListener('click', function(e) {
+    e.preventDefault();
+    clicks++;
+    p.innerHTML="Clicks:"+clicks;
+  });
+}
